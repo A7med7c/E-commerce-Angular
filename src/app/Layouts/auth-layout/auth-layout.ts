956e1a59a708acf0acb00f555f059a8ea3b99fb3
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-04-16 20:51:22 +0200

    Configure Routing

---
+++ src/app/Layouts/auth-layout/auth-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavAuth } from "../../Components/nav-auth/nav-auth";
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-auth-layout',
+  imports: [NavAuth, RouterOutlet],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.scss',
+})
+export class AuthLayout { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './Layouts/auth-layout/auth-layout';
+import { Login } from './Components/login/login';
+import { Register } from './Components/register/register';
+import { BlankLayout } from './Layouts/blank-layout/blank-layout';
+import { Home } from './Components/home/home';
+import { Product } from './Components/product/product';
+import { Category } from './Components/category/category';
+import { Cart } from './Components/cart/cart';
+import { Brand } from './Components/brand/brand';
+import { NotFound } from './Components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: AuthLayout, children: [
+            { path: "", component: Login },
+            { path: "login", component: Login },
+            { path: "register", component: Register },
+        ]
+    },
+    {
+        path: "", component: BlankLayout, children: [
+            { path: "", component: Home },
+            { path: "home", component: Home },
+            { path: "products", component: Product },
+            { path: "categories", component: Category },
+            { path: "cart", component: Cart },
+            { path: "brands", component: Brand },
+        ]
+    },
+    { path: "**", component: NotFound }
+];
