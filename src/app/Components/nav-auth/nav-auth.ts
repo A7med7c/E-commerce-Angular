e5c620496cb61baded9ef3268c9d960534281506
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-04-16 16:52:51 +0200

    Create All Components

---
+++ src/app/Components/nav-auth/nav-auth.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-auth',
+  imports: [],
+  templateUrl: './nav-auth.html',
+  styleUrl: './nav-auth.scss',
+})
+export class NavAuth {}
