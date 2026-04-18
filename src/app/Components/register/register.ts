bc5c084cbf0b5a0f11587ce057247b8d68aaee87
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-04-18 18:06:12 +0200

    Implement registration form with validation and update layouts to include footer

--- src/app/Components/register/register.ts
+++ src/app/Components/register/register.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  //registerForm : is the form group that represent object need to be sent to backend
+  registerForm: FormGroup = new FormGroup({
+    // each form control represent property in the json 
+    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+    rePassword: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  })
+
+
+  register(): void {
+    console.log(this.registerForm)
+  }
+}
