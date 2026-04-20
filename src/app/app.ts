b274eae22cb188b20ea29719152394de7bf38a4c
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-04-20 15:26:07 +0200

    Enhance product display and loading state: update product card structure with star ratings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App { }
+export class App {
+  protected readonly title = signal('E-commerce-Angular');
+  isReady = false;
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.isReady = true;
+    }
+  }
+}
