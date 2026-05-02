d97d1cbbc0fbaf0e38fa73fde26f0a268a9c1fa6
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-05-02 09:49:08 +0300

    Add footer translation support

--- src/app/Components/footer/footer.ts
+++ src/app/Components/footer/footer.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { TranslationService } from '../../Core/Services/translation-service';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [TranslatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer implements OnInit {
+  private readonly _translationService = inject(TranslationService);
+
+  currentLang: string = 'en';
+
+  ngOnInit(): void {
+    this.currentLang = this._translationService.getCurrentLang();
+
+    this._translationService.getCurrentLang$().subscribe(() => {
+      this.currentLang = this._translationService.getCurrentLang();
+    });
+  }
+}
