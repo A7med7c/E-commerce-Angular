a1dbec77296cca7d5da688c76bfd0790473559ec
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-05-03 22:26:27 +0300

    Refactor routing to use lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,49 +1,96 @@
 import { Routes } from '@angular/router';
-import { AuthLayout } from './Layouts/auth-layout/auth-layout';
-import { BlankLayout } from './Layouts/blank-layout/blank-layout';
-
-import { Login } from './Components/login/login';
-import { Register } from './Components/register/register';
-import { Home } from './Components/home/home';
-import { Product } from './Components/product/product';
-import { Category } from './Components/category/category';
-import { Cart } from './Components/cart/cart';
-import { Brand } from './Components/brand/brand';
-import { NotFound } from './Components/not-found/not-found';
-
 import { authGuard } from './Core/Guards/auth-guard';
 import { isLoggedGuard } from './Core/Guards/is-logged-guard';
-import { Details } from './Components/details/details';
-import { ForgetPassword } from './Components/forget-password/forget-password';
-import { PlaceOrder } from './Components/place-order/place-order';
-import { AllOrders } from './Components/all-orders/all-orders';
 
 export const routes: Routes = [
+
     {
         path: "",
-        component: AuthLayout,
+        loadComponent: () =>
+            import('./Layouts/auth-layout/auth-layout').then(m => m.AuthLayout),
         canActivate: [isLoggedGuard],
         children: [
             { path: "", redirectTo: "login", pathMatch: "full" },
-            { path: "login", component: Login },
-            { path: "register", component: Register },
-            { path: "forgetpassword", component: ForgetPassword },
+
+            {
+                path: "login",
+                loadComponent: () =>
+                    import('./Components/login/login').then(m => m.Login)
+            },
+            {
+                path: "register",
+                loadComponent: () =>
+                    import('./Components/register/register').then(m => m.Register)
+            },
+            {
+                path: "forgetpassword",
+                loadComponent: () =>
+                    import('./Components/forget-password/forget-password').then(m => m.ForgetPassword)
+            },
         ],
     },
+
     {
         path: "",
-        component: BlankLayout,
+        loadComponent: () =>
+            import('./Layouts/blank-layout/blank-layout').then(m => m.BlankLayout),
         canActivate: [authGuard],
         children: [
-            { path: "home", component: Home },
-            { path: "products", component: Product },
-            { path: "categories", component: Category },
-            { path: "cart", component: Cart },
-            { path: "brands", component: Brand },
-            { path: "details/:id", component: Details },
-            { path: "orders/:id", component: PlaceOrder },
-            { path: "allorders", component: AllOrders },
+
+            {
+                path: "home",
+                loadComponent: () =>
+                    import('./Components/home/home').then(m => m.Home)
+            },
+
+            {
+                path: "products",
+                loadComponent: () =>
+                    import('./Components/product/product').then(m => m.Product)
+            },
+
+            {
+                path: "categories",
+                loadComponent: () =>
+                    import('./Components/category/category').then(m => m.Category)
+            },
+
+            {
+                path: "cart",
+                loadComponent: () =>
+                    import('./Components/cart/cart').then(m => m.Cart)
+            },
+
+            {
+                path: "brands",
+                loadComponent: () =>
+                    import('./Components/brand/brand').then(m => m.Brand)
+            },
+
+            {
+                path: "details/:id",
+                loadComponent: () =>
+                    import('./Components/details/details').then(m => m.Details)
+            },
+
+            {
+                path: "orders/:id",
+                loadComponent: () =>
+                    import('./Components/place-order/place-order').then(m => m.PlaceOrder)
+            },
+
+            {
+                path: "allorders",
+                loadComponent: () =>
+                    import('./Components/all-orders/all-orders').then(m => m.AllOrders)
+            },
+
         ],
     },
-    { path: "**", component: NotFound },
+
+    {
+        path: "**",
+        loadComponent: () =>
+            import('./Components/not-found/not-found').then(m => m.NotFound)
+    },
 ];
